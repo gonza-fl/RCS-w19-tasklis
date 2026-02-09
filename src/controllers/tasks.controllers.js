12881e525969f8a593e61e33c6b02362cf197343
Gonza-Fl() <gonzaloflamenci4@gmail.com>
2026-02-09 17:37:30 -0300

    feat: implementar restauracion de tareas y notas sobre autenticacion/autorizacion

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,8 +1,14 @@
 import { Task } from "../models/Task.js"
 
-const listAllTasks = async (_req, res) => {
+const listAllTasks = async (req, res) => {
+
+    const { getRecycleBin } = req.query;
+
     try {
-        const tasks = await Task.find()
+        if (getRecycleBin === 'true') {
+            return recycleBin(req, res)
+        }
+        const tasks = await Task.find({ deletedAt: null })
         return res.status(200).json(tasks)
     } catch (error) {
         return res.status(500).json({ error: error.message })
@@ -12,7 +18,7 @@ const listAllTasks = async (_req, res) => {
 const getTaskById = async (req, res) => {
     try {
         const task = await Task.findById(req.params.id)
-        if (!task) {
+        if (!task || task.deletedAt === null || task.deletedAt === undefined) {
             return res.status(404).json({ error: 'Task not found' })
         }
         return res.status(200).json(task)
@@ -43,14 +49,46 @@ const patchTaskById = async (req, res) => {
 }
 
 const deleteTaskById = async (req, res) => {
+    const softDelete = req.query.softDelete === 'true'
+    try {
+        if (softDelete) {
+            const task = await Task.findById(req.params.id)
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' })
+            }
+            task.deletedAt = new Date()
+            await task.save()
+            return res.status(200).json({ message: 'Task deleted successfully' })
+        } else {
+            const task = await Task.findByIdAndDelete(req.params.id)
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' })
+            }
+            return res.status(200).json({ message: 'Task deleted successfully' })
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const restoreTaskById = async (req, res) => {
     try {
         const task = await Task.findById(req.params.id)
         if (!task) {
             return res.status(404).json({ error: 'Task not found' })
         }
-        task.deletedAt = new Date()
+        task.deletedAt = null
         await task.save()
-        return res.status(200).json({ message: 'Task deleted successfully' })
+        return res.status(200).json({ message: 'Task restored successfully' })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const recycleBin = async (_req, res) => {
+    try {
+        const tasks = await Task.find({ deletedAt: { $ne: null } })
+        return res.status(200).json(tasks)
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
@@ -62,4 +100,6 @@ export {
     getTaskById,
     patchTaskById,
     deleteTaskById,
+    restoreTaskById,
+    recycleBin
 }
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { listAllTasks, createTask, getTaskById, patchTaskById, deleteTaskById } from '../controllers/tasks.controllers.js';
+import { listAllTasks, createTask, getTaskById, patchTaskById, deleteTaskById, restoreTaskById, recycleBin } from '../controllers/tasks.controllers.js';
 import { validateCreateTask, validateTaskId, validateUpdateTaskById } from '../middlewares/tasks.middleware.js';
 
 const router = Router();
@@ -9,5 +9,11 @@ router.get('/:id', validateTaskId, getTaskById);
 router.patch('/:id', validateUpdateTaskById, patchTaskById);
 router.delete('/:id', validateTaskId, deleteTaskById);
 router.post('/', validateCreateTask, createTask);
+router.patch('/:id/restore', validateTaskId, restoreTaskById);
 
 export default router;
+
+// TODO: AGREGAR AUTHENTICATION & AUTHORIZATION
+// Autenticacion vs Autorizacion
+// Autenticacion: Quien sos? (Login, JWT, Tokens)
+// Autorizacion: Que podes hacer? (Roles, Permisos)
